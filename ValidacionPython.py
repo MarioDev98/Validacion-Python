10d281b7e9185392d85aa6e1b064348e677afacb
MarioIsaiMedinaLucero <mariomedinalucero29@gmail.com>
2023-04-14 10:04:22 -0600

    First commit

---
+++ ValidacionPython.py
@@ -0,0 +1,12 @@
+contraseña = "Retoexamen"
+tarjeta = "1234 6772 2727 9912"
+numero=int(3)
+for i in range(numero):
+  usuario_res = input("Cual es tu contraseña? ")
+  
+if usuario_res != contraseña:
+    print("Se ha excedido el numero de intentos")
+else:
+    print("**** **** **** 9912")
+
+
